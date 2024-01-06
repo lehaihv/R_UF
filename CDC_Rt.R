cdcb97ea96f5296b78b0764100ce44e738f5fc0e
Hai LE <69892340+lehaihv@users.noreply.github.com>
2024-01-06 15:45:17 +0700

    Mac 7740

--- CDC_Rt.R
+++ CDC_Rt.R
@@ -6,8 +6,8 @@ library(writexl)
 
 ## load data
 ## data1 <- read_excel("D:/GitHub/R_UF/covid_29019_cc.xlsx")
-data_cc <- read_excel("~/Documents/GitHub/R_UF/covid_29019_cc.xlsx")  ## After Dec 2021
-data_ww <- read_excel("~/Documents/GitHub/R_UF/covid_29019_ww.xlsx")
+data_cc <- read_excel("~/Documents/GitHub/R_UF/covid_264_cc.xlsx")  ## After Dec 2021
+data_ww <- read_excel("~/Documents/GitHub/R_UF/covid_264_ww.xlsx")
 # data_cc <- read_excel("~/Documents/GitHub/R_UF/covid_29019_cc_omicron.xlsx") ## Before Dec 2021
 # data_ww <- read_excel("~/Documents/GitHub/R_UF/covid_29019_ww_omicron.xlsx")
 
@@ -60,22 +60,22 @@ data_ww$dates = date_only1
 res_parametric_si_cc <- estimate_R(data_cc,
                                 method="parametric_si",
                                 config = make_config(list(
-                                mean_si = 2.6,
-                                std_si = 1.5))
+                                mean_si = 4.7,
+                                std_si = 2.9))
 )
-plot(res_parametric_si_cc, legend = FALSE, "R") ## "R" only plot Estimated Rt
+plot(res_parametric_si_cc, legend = FALSE, "incid") ## "R" only plot Estimated Rt
 
 res_parametric_si_ww <- estimate_R(data_ww,
                                 method="parametric_si",
                                 config = make_config(list(
-                                mean_si = 2.6,
-                                std_si = 1.5))
+                                mean_si = 4.7,
+                                std_si = 2.9))
 )
 plot(res_parametric_si_ww, legend = FALSE, "R") ## "R" only plot Estimated Rt
 
 ## visualize R estimates on the same plot
-estimate_R_plots(list(res_parametric_si_cc, res_parametric_si_ww), what = "R",
-                 options_R = list(col = c("blue", "red")), legend = TRUE)
+estimate_R_plots(list(res_parametric_si_cc, res_parametric_si_ww), what = "incid",
+                 options_I = list(col = c("blue", "red")), legend = TRUE)
 
 
 ## wrtie value of estimated R to excel file
